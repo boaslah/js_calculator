4c2fd528781c28f14fc9a1445fdb6438c23bd709
Blessing Boaslah <blessing@kwagei.com>
2020-08-26 08:34:49 +0000

    refactor the javascript file

--- calculator.js
+++ calculator.js
@@ -1,16 +1,13 @@
 
 // var = numberElement
 var cal = document.getElementById("screen");
-
 var val = cal.value;
 
-// Show 
-function showBtnOne()
+// Function to show button
+function showButton(val)
 {
     var input = cal.value;
-
-    var toDisplay = document.getElementById("num1").innerHTML;
-
+    var toDisplay = document.getElementById(val.id).innerHTML;
     cal.value = input + toDisplay; 
 }
 
@@ -19,144 +16,6 @@ function clearInputEl()
 {
     cal.value = " ";
 }
-
-// showBtnTwo
-function showBtnTwo()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num2").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnThree
-function showBtnThree()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num3").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnFour
-function showBtnFour()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num4").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnFive
-function showBtnFive()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num5").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnSix
-function showBtnSix()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num6").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnSeven
-function showBtnSeven()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num7").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnEight
-function showBtnEight()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num8").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnNine
-function showBtnNine()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num9").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-// showBtnZero
-function showBtnZero()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("num0").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-
-
-function showBtnAdd()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("add").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-function showBtnSub()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("sub").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-function showBtnMulti()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("multi").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-function showBtnDivi()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("divi").innerHTML;
-
-    cal.value = input + toDisplay; 
-}
-
-function showBtnPoint()
-{
-    var input = cal.value;
-
-    var toDisplay = document.getElementById("point").innerHTML;
-
-    cal.value =  input + toDisplay; 
-}
-
 // A function that add numbers 
 function addition ()
 {
