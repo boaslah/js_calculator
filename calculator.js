24e82f416a60dae7a4e837b8c86d678584465372
Blessing Boaslah <blessing@kwagei.com>
2020-08-26 09:28:50 +0000

    refactor css file

--- calculator.js
+++ calculator.js
@@ -10,7 +10,6 @@ function showButton(val)
     var toDisplay = document.getElementById(val.id).innerHTML;
     cal.value = input + toDisplay; 
 }
-
 // Clear the input element 
 function clearInputEl()
 {
@@ -21,79 +20,53 @@ function addition ()
 {
     var final = cal.value.split('+');
     console.log(final);
-
     var a = Number(final[0]);
-
     var b = Number(final[1]);
-    
     console.log(a, b);
     console.log(a + b);
     var result = a + b;
 
     cal.value = result;
 }
-
 // A function that multiply numbers 
-
 function multiplication ()
 {  
     var final = cal.value.split('*')
-
     var a = Number(final[0]);
-
     var b = Number(final[1]);
-
     var result = a * b;
-
     cal.value = result;
 }
-
 // A function that substract numbers 
-
 function substraction ()
 {
     var final = cal.value.split('-');
-
     var a = Number(final[0]);
-
     var b = Number(final[1]);
-
     var result = a - b;
-
     cal.value = result;
 }
-
  // A function that divide numbers 
-
 function division ()
 {
     var final = cal.value.split('/');
-
     var a = Number(final[0]);
-
     var b = Number(final[1]);
-
     var result = a / b;
-
     cal.value = result;
 }
 function delect()
 {
     var x = cal.value;
-
     if (x.length > 0)
     {
         x = x.substring(0, x.length-1);
-        
         cal.value = x;
     }
 }
-
-
 function displayResult()
 {
     saveResult = cal.value;
-
     if(saveResult.includes("+"))
     {
         addition();
